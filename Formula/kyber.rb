ea0291243f0912e9f60b8fb4c2aba37900b57c0f
Ian Gordon <ian@aptos.bike>
2026-03-06 13:34:24 -0800

    Fix formula for private repo: use API URLs with auth headers

--- Formula/kyber.rb
+++ Formula/kyber.rb
@@ -6,11 +6,13 @@ class Kyber < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/IanGordonOne/kyber/releases/download/v0.9.0/kyber-0.9.0-darwin-arm64.tar.gz"
+      url "https://api.github.com/repos/IanGordonOne/kyber/releases/assets/368524841",
+          headers: ["Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}", "Accept: application/octet-stream"]
       sha256 "fa304ac2b6bdd84eda1e0317d0a3156256b6d03dad81d5f49a3e591f3c0775e1"
     end
     on_intel do
-      url "https://github.com/IanGordonOne/kyber/releases/download/v0.9.0/kyber-0.9.0-darwin-amd64.tar.gz"
+      url "https://api.github.com/repos/IanGordonOne/kyber/releases/assets/368524838",
+          headers: ["Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}", "Accept: application/octet-stream"]
       sha256 "862239a8830b8dd7731f5bd2700a3198993762c3d0f089d7efc9ba3e3feffe86"
     end
   end
